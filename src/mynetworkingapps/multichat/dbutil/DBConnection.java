60098a1016ca3c89573048f4ef0835676f132133
abhiraj <abhiajsinghtomar@gmail.com>
2018-04-02 05:31:50 -0700

    2/4/2018

--- src/mynetworkingapps/multichat/dbutil/DBConnection.java
+++ src/mynetworkingapps/multichat/dbutil/DBConnection.java
@@ -5,12 +5,43 @@
  */
 package mynetworkingapps.multichat.dbutil;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author ABHIRAJ
  */
 public class DBConnection {
     
+    private static Connection conn;
     
+    static {
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+            
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@//Sachin-PC/orcl","chatdata","chatapp");
+            JOptionPane.showMessageDialog(null,"Connected succesfull to the database","Success!",JOptionPane.INFORMATION_MESSAGE);
+            
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+    }
     
+    public static Connection getConnection() {
+        return conn;
+    }
+    
+    public static void closeConnection() {
+        try {
+            if(conn != null) 
+                conn.close();
+            JOptionPane.showMessageDialog(null,"Disconnectd successfully from the database","Success!",JOptionPane.INFORMATION_MESSAGE);
+        }
+        catch(Exception e) {
+            JOptionPane.showMessageDialog(null,"Error Disconnection from the database" + e,"Error!",JOptionPane.ERROR_MESSAGE);
+        }
+    }
 }
